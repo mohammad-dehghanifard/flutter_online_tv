77e11db1db3f3155efbd4edf5d9577aa8adefb3b
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-04 22:17:28 -0700

    upload project

---
+++ android/app/src/main/kotlin/ir/dehghanifard/flutter_online_tv/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.dehghanifard.flutter_online_tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors{
+  AppColors._();
+
+  static Color scaffoldBg = const Color(0xFF000000);
+  static Color lightBlue = const Color(0xFF00B4A2);
+  static Color gray = const Color(0xFF222222);
+}
